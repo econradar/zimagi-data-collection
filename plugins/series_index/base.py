d6c408afedac07bd5caa8df80aa34fd94b63fe40
Adrian Webb <adrian@webb.sh>
2022-12-03 14:27:15 -0500

    Updating the data facade handling in the base series_index plugin provider.

--- plugins/series_index/base.py
+++ plugins/series_index/base.py
@@ -4,5 +4,14 @@ from systems.plugins.index import BasePlugin
 class BaseProvider(BasePlugin('series_index')):
 
     @property
+    def data_name(self):
+        return self.name
+
+    @property
     def data(self):
-        return self.command.facade(self.name, False)
+        if not getattr(self, '_data_facade', None):
+            self._data_facade = self.command.facade(self.data_name, False)
+            self._data_facade.set_scope({
+                'series_index_id': self.instance.get_id()
+            })
+        return self._data_facade
